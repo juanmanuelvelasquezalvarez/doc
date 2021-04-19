99beb05de0f879f0ee91edd01ee9e45cb2684ddb
juanmanuelvelasquezalvarez <juanmanuelv@une.net.co>
2021-04-18 19:38:58 -0500

    Add files via upload

---
+++ ideal.rb
@@ -0,0 +1,96 @@
+#If the year has or not 1 week more
+def days(y)
+	y==((y*1.242189/7).round*7/1.242189).round ? 371 : 364
+end
+#Milliseconds from the counting to the glorious change of millennium 01/01/2000.
+#The Gregorian 1/1/2000 would be 7/1/2000 in the ideal calendar, a standard I propose.
+class Ideal
+    include Comparable
+    attr_accessor:D
+	@@f = true
+	def initialize(g=Time.new)
+		@D=0#Days since the change of millennium
+		@y=2000#Year initiating in 2000
+		@d=6#Day of the year counting since 0 (Sunday), 6 is Saturday, day the change of millennium was.
+		n=(g-Time.local(2000, 1, 1))/86400
+		addDays(n.floor)#From Gregorian to ideal calendar
+	end
+	def addDays(n)
+		@d+=n
+		if n>0
+			a=days(@y)
+			while @d>=a
+				@d-=a
+				@y+=1
+				a=days(@y)
+			end
+		else
+			while @d<0
+				@y-=1
+				@d+=days(@y)
+			end
+		end
+		@D+=n
+	end
+	def addYears(n)
+		a=@y+n
+		if @d>363
+			if days(a)==364
+				@d-=7
+				@D-=7
+			end
+		end
+		if n>0
+			while @y<a
+				@D+=days(@y)
+				@y+=1
+			end
+		elsif n<0
+			while @y>a
+				@y-=1
+				@D-=days(@y)
+			end
+		end
+	end
+    def <=>(o)
+        return self.D<=>o.D
+    end
+    def >(o)
+        return self.D>o.D
+    end
+    def <(o)
+        return self.D<o.D
+    end
+    def ==(o)
+        return self.D==o.D
+    end
+	#Day, week and month start at 0 internally and here 1 is added.
+	def y
+		@y
+	end
+	def m
+		(@d/28)+1
+	end
+	def w
+		(@d/7)+1
+	end
+	def d
+		@d+1
+	end
+	def dm
+		(@d%28)+1
+	end
+	def dw
+		(@d%7)+1
+	end
+	def D
+		@D
+	end
+	def gregorian
+		Time.new(2000,1,1)+@D*86400
+	end
+	def to_s
+		"#{y}#{@@f=nil ? d : " #{@@f ? m : w} #{@@f ? dm : dw}"}"
+	end
+end
+puts Ideal.new
