2a5e6f89531727ce30d7fbff482e1eab2b12a10f
juanmanuelvelasquezalvarez <juanmanuelv@une.net.co>
2021-04-20 13:18:04 -0500

    Update Ideal.cs

--- Ideal.cs
+++ Ideal.cs
@@ -2,60 +2,93 @@ using System;
 namespace Ideal
 {
 	class Ideal
-    	{
+	{
 		//If the year has or not 1 week more
 		public static int days(int y) => y == Math.Round(Math.Round(y * 1.242189 / 7) * 7 / 1.242189) ? 371 : 364;
 		private int D,//Days since the change of millennium initiating in 0.
-			y = 2000,//Year initiating in 2000
-			d = 6;//Day of the year counting since 0 (Sunday), 6 is Saturday, day that the change of millennium was.
+			_y = 2000,//Year initiating in 2000
+			_d;//Day of the year counting since 0 (Sunday), 6 is Saturday, day that the change of millennium was.
 		//The Gregorian 1/1/2000 would be 7/1/2000 in the ideal calendar, a standard I propose.
-		//From Gregorian to ideal calendar
-		public Ideal(DateTime g)
+		public Ideal(DateTime g)//From Gregorian to ideal calendar
 		{
-			AddDays((int)g.Subtract(DateTime.Parse("2000-01-01")).TotalDays);
+			d=6+(int)g.Subtract(DateTime.Parse("2000-01-01")).TotalDays;
 		}
 		//Year and day. If d>days(y), days pass to some posterior year. If d<1, days pass to some previous year.
-		public Ideal(int y, int d)
+		public Ideal(int Y, int D)
 		{
-			AddYears(y - 2000); AddDays(d - 7);
+			y=Y; d=D;
 		}
-		public void AddDays(int n)
+		public int d
 		{
-			d += n;
-			if (n > 0)
+			set
 			{
-				int N = days(y);
-				while (d >= N)
+				D += value-_d;
+				_d = value;
+				int N = days(_y);
+				while (_d >= N)
 				{
-					d -= N;
-					N = days(++y);
+					_d -= N;
+					N = days(++_y);
 				}
+				while (_d < 0) _d += days(--_y);
 			}
-			else while (d < 0) d += days(--y);
-			D += n;
+            get
+            {
+				return _d;
+            }
 		}
-		public void AddYears(int n)
+		public int y
 		{
-			int a = y + n;
-			if (d > 363) if (days(a) == 364)
+			set
+			{
+				if (_d > 363) if (days(value) == 364)
 				{
-					d -= 7; D -= 7;
+					_d -= 7; D -= 7;
 				}
-			if (n > 0) while (y < a) D += days(y++);
-			if (n < 0) while (y > a) D -= days(--y);
+				while (_y < value) D += days(_y++);
+				while (_y > value) D -= days(--_y);
+			}
+			get
+            {
+				return _y;
+            }
 		}
-		public static bool? f;
+		//Day, week and month start at 0 internally and here 1 is added.
+		public int m
+        {
+            get
+            {
+				return _d / 28 + 1;
+            }
+		}
+		public int w
+		{
+			get
+			{
+				return _d / 7 + 1;
+			}
+		}
+		public int dm
+		{
+			get
+			{
+				return _d % 28 + 1;
+			}
+		}
+		public int dw
+		{
+			get
+			{
+				return _d % 7 + 1;
+			}
+		}
+		public static bool? f=true;
 		public override string ToString()
 		{
-			if(f==null) return (d + 1) + " " + y;
+			if(f==null) return (_d + 1) + " " + _y;
 			bool b = (bool)f;
-			return (b?5:6)+" "+get(b?2:3)+" "+y;
+			return (b?dm:dw)+" "+(b?m:w)+" "+_y;
 		}
-		//Choose among year, month, week, day of the year/month/week.
-		//Day, week and month start at 0 internally and here 1 is added.
-		public int get(int f) =>
-			f < 1 | f > 6 ? 0 : f == 1 ? y : (f == 2 ? d / 28 : f == 3 ? d / 7 : f == 4 ? d : f == 5 ? d % 28 : d % 7) + 1;
-		public static explicit operator int(Ideal i) => i.D;
 		//If this date is previous or posterior than another.
 		public static bool operator >(Ideal a, Ideal b) => a.D > b.D;
 		public static bool operator <(Ideal a, Ideal b) => a.D < b.D;
@@ -66,11 +99,17 @@ namespace Ideal
 		//From ideal to Gregorian calendar
 		public static explicit operator DateTime(Ideal i) => DateTime.Parse("2000-01-01").AddDays(i.D);
 		public static explicit operator string(Ideal i) => i.ToString();
+		public static explicit operator int(Ideal i) => i.D;
 		static void Main(string[] args)
 		{
 			Console.WriteLine(new Ideal(DateTime.Now));
 		}
-		public override bool Equals(object o) => this==(Ideal)o;
+		public override bool Equals(object o)
+		{
+			if (o.GetType().Equals(typeof(Ideal))) return this==(Ideal)o;
+			if (o.GetType().Equals(typeof(int))) return this == (int)o;
+			return true;
+		}
 		public override int GetHashCode() => base.GetHashCode();
 	}
 }
