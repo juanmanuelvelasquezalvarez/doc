c45557bc9055d90b56bbb0e9dd92c3a336042491
juanmanuelvelasquezalvarez <juanmanuelv@une.net.co>
2021-04-20 13:22:17 -0500

    Update Ideal.php

--- Ideal.php
+++ Ideal.php
@@ -5,57 +5,52 @@ function days(int $y) {
 }
 class Ideal {
 	private $D;//Days since the change of millennium initiating in 0.
-	private $y=2000;//Year initiating in 2000
-	private $d=6;//Day of the year counting since 0 (Sunday), 6 is Saturday, day that the change of millennium was.
+	private $_y=2000;//Year initiating in 2000
+	private $_d;//Day of the year counting since 0 (Sunday), 6 is Saturday, day that the change of millennium was.
 	//The Gregorian 1/1/2000 would be 7/1/2000 in the ideal calendar, a standard I propose.
 	public function __construct($g=null) {
-		$this->addDays(((int)($g==null?date("U"):$g)-(int)date("U",mktime(0,0,0,1,1,2000)))/86400);
-	}
-	public function addDays(int $n) {
-		$this->d+=$n;
-		if($n>0) {
-			$N=days($this->y);
-			while($this->d>=$N) {
-				$this->d-=$N;
-				$N=days(++$this->y);
-			}
-		}
-		else while($this->d<0) $this->d+=days(--$this->y);
-		$this->D+=$n;
-	}
-	public function addYears(int $n) {
-		$a=$this->y+$n;
-		if($this->d>363) if(days($a)==364) {
-			$this->d-=7;
-			$this->D-=7;
-		}
-		if($n>0) while($this->y<$a) $this->D+=days($this->y++);
-		if($n<0) while($this->y>$a) $this->D-=days(--$this->y);
+		$this->d=6+((int)($g==null?date("U"):$g)-(int)date("U",mktime(0,0,0,1,1,2000)))/86400;
 	}
 	//Choose among year, month, week, day of the year/month/week.
 	//Week and month start at 0 internally and here 1 is added.
 	public function __get($f) {
 		switch($f) {
-			case 'y': return $this->y;
-			case 'm': return floor($this->d/28)+1;
-			case 'w': return floor($this->d/7)+1;
-			case 'd': return $this->d+1;
+			case 'y': return $this->_y;
+			case 'm': return floor($this->_d/28)+1;
+			case 'w': return floor($this->_d/7)+1;
+			case 'd': return $this->_d;
 			case 'D': return $this->D;
-			case 'dm': return $this->d%28+1;
-			case 'dw': return $this->d%7+1;
+			case 'dm': return $this->_d%28+1;
+			case 'dw': return $this->_d%7+1;
 			//Ideal calendar to Gregorian.
 			case 'g': return date_add(date_create("2000-01-01"),date_interval_create_from_date_string($this->D.' days'));
 		}
 		return 0;
 	}
-	//Year and day. If d>days(y), days pass to some posterior year. If d<1, days pass to some previous year.
+	//If d>days(y), days pass to some posterior year. If d<1, days pass to some previous year.
 	public function __set($n,$v) {
-		if($n=='y') $this->addYears($v-$this->y);
-		else if($n=='d') $this->addDays($v-$this->d-1);
+		if($n=='d') {
+			$this->D+=$v-$this->_d;
+			$this->_d=$v;
+			$N=days($this->_y);
+			while($this->_d>=$N) {
+				$this->_d-=$N;
+				$N=days(++$this->_y);
+			}
+			while($this->_d<0) $this->_d+=days(--$this->_y);
+		}
+		else if($n=='y') {
+			if($this->_d>363) if(days($v)==364) {
+				$this->_d-=7;
+				$this->D-=7;
+			}
+			while($this->_y<$v) $this->D+=days($this->_y++);
+			while($this->_y>$v) $this->D-=days(--$this->_y);
+		}
 	}
 	public static $f=true;
 	public function __toString() {
-		return (self::$f instanceof bool)?($this->d+1):((self::$f?$this->dm:$this->dw)." ".(self::$f?$this->m:$this->w))." ".$this->y;
+		return ((self::$f=true|self::$f=false)?((self::$f?$this->dm:$this->dw)." ".(self::$f?$this->m:$this->w)):$this->d+1)." ".$this->y;
 	}
 }
 echo new Ideal();//Now
